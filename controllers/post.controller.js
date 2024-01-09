2df3047facc42a349396ba2e68194898108d18b9
Audrey-kirezi <audreykirezi100@gmail.com>
2024-01-09 14:08:08 +0200

    Updating post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -24,3 +24,31 @@ exports.createPost = async (req, res) => {
         return serverErrorResponse(ex, res);
     }
 };
+exports.updatePost = async (req, res) =>{
+    try {
+        
+        const { postId } = req.params;
+        const { title, description, fileUrl } = req.body;
+        const existingPost = await Post.findById(postId);
+
+        if (!existingPost) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        if (existingPost.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Permission denied. You are not the creator of this post." });
+        }
+        const updatedPost = await Post.findByIdAndUpdate(
+          postId,
+          { title, description, fileUrl },
+          { new: true }
+        );
+    
+        if (!updatedPost) {
+          return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.status(200).json(updatedPost);
+    }catch (ex) {
+        return serverErrorResponse(ex, res);
+    }
+}
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createPost } = require("../controllers/post.controller");
+const { createPost,updatePost } = require("../controllers/post.controller");
 const authenticate = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/create", authenticate, createPost);
+router.put('/update/:postId', authenticate, updatePost)
 
 module.exports = router;
