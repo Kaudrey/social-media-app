76a41992f7a71e5c901e82d18c7b580cdabcec09
Audrey-kirezi <audreykirezi100@gmail.com>
2024-01-13 13:07:01 +0200

    sending Email

--- utils/emailConfig.js
+++ utils/emailConfig.js
@@ -2,22 +2,21 @@ const nodemailer = require('nodemailer');
 
 const mailTransporter = nodemailer.createTransport({
   service: 'gmail',
-//   host:'smtp.gmail.com',
   port: 587,
   secure: true,
   auth: {
-    user: 'audreykirezi100@gmail',
-    pass: 'dsea ekjq bsgc vdqm'
+    user: process.env.USER,
+    pass: process.env.PASS
   }
 });
 
 async function sendWelcomeEmail(email, username) {
   try {
     const mailDetails = {
-      from: 'audreykirezi100@gmail.com',
+      from: process.env.USER,
       to: email,
-      subject: 'Welcome to YourApp!',
-      text: `Hello ${username},\n\nThank you for registering on YourApp! We're excited to have you.`,
+      subject: 'Welcome to our app',
+      text: `Hello ${username},\n\nThank you for registering on our socail media app!`,
     };
 
     await mailTransporter.sendMail(mailDetails);
